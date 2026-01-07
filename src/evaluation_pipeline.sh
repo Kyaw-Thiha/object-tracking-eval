70439c47c71b4129012e48de2eaa7956ba81495f
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2026-01-07 10:01:44 -0500

    Fixed the paths

--- src/evaluation_pipeline.sh
+++ src/evaluation_pipeline.sh
@@ -10,12 +10,28 @@ TRACKER="probabilistic_byte_tracker"
 MODEL_FACTORY="yolox_noise"
 # MODEL_FACTORY="prob_yolox"
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SRC_ROOT="$SCRIPT_DIR"
+
+# Allow callers to override the default roots to avoid hard-coded absolute paths.
+DATA_ROOT="${DATA_ROOT:-$SRC_ROOT/data}"
+OUTPUT_ROOT="${OUTPUT_ROOT:-$SRC_ROOT/outputs}"
+EVAL_RESULT_ROOT="${EVAL_RESULT_ROOT:-$SRC_ROOT/evaluation_results}"
+PLOT_ROOT="${PLOT_ROOT:-$SRC_ROOT/plots}"
+
+DATASET_DIR="${DATA_ROOT}/${DATASET}_dataset/test"
+OUTPUT_DIR="${OUTPUT_ROOT}/${DATASET}_${MODEL_FACTORY}_${TRACKER}"
+EVAL_RESULT_DIR="${EVAL_RESULT_ROOT}/${DATASET}_${MODEL_FACTORY}_${TRACKER}"
+PLOT_SAVE_PATH="${PLOT_ROOT}/${DATASET}_${MODEL_FACTORY}_${TRACKER}"
+
+mkdir -p "$OUTPUT_DIR" "$EVAL_RESULT_DIR" "$PLOT_SAVE_PATH"
+
 python -X faulthandler evaluation_pipeline.py \
     --dataloader_factory "${DATASET}_factory.py" \
-    --dataset_dir "/home/allynbao/project/UncertaintyTrack/src/data/${DATASET}_dataset/test/" \
+    --dataset_dir "$DATASET_DIR" \
     --model_factory "${MODEL_FACTORY}.py" \
     --tracker "${TRACKER}" \
     --device "${DEVICE}" \
-    --output_dir "/home/allynbao/project/UncertaintyTrack/src/outputs/${DATASET}_${MODEL_FACTORY}_${TRACKER}/" \
-    --eval_result_dir "/home/allynbao/project/UncertaintyTrack/src/evaluation_results/${DATASET}_${MODEL_FACTORY}_${TRACKER}/" \
-    --plot_save_path "/home/allynbao/project/UncertaintyTrack/src/plots/${DATASET}_${MODEL_FACTORY}_${TRACKER}/" \
+    --output_dir "$OUTPUT_DIR" \
+    --eval_result_dir "$EVAL_RESULT_DIR" \
+    --plot_save_path "$PLOT_SAVE_PATH" \
