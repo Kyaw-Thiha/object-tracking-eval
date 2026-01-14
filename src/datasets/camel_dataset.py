d6c083024dcd746ac5d754a79e42e3da15af3e52
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2026-01-14 14:53:40 -0500

    Updated the camel dataset

--- src/datasets/camel_dataset.py
+++ src/datasets/camel_dataset.py
@@ -1,9 +1,9 @@
 import os
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from pycocotools.coco import COCO
 import numpy as np
-import cv2
+import cv2 as cv
 
 
 class CAMELCocoDataset(Dataset):
@@ -12,17 +12,19 @@ class CAMELCocoDataset(Dataset):
         self.img_prefix = img_prefix
         self.input_size = input_size
 
-        # group images by video (MOT17 uses video sequence in filename prefix)
+        # Image IDs
         self.ids = list(sorted(self.coco.imgs.keys()))
+
+        # Consistent category mapping (same as MOT17 logic)
         self.cat_id_to_label = {cat_id: i for i, cat_id in enumerate(self.coco.getCatIds())}
 
-        # keep mapping: image_id -> video_name, frame_id
+        # image_id -> (video_id, frame_id)
         self.frame_info = {}
         for img_id in self.ids:
             img_info = self.coco.loadImgs(img_id)[0]
-            # camel COCO stores video_id and frame ids in the JSON
-            video_id = img_info.get("video_id")
-            frame_id = img_info.get("mot_frame_id", img_info.get("frame_id"))
+            video_id = img_info["video_id"]
+            frame_id = img_info["mot_frame_id"]  # use CAMEL frame index
+
             self.frame_info[img_id] = dict(video_id=video_id, frame_id=frame_id)
 
     def __len__(self):
@@ -30,58 +32,67 @@ class CAMELCocoDataset(Dataset):
 
     def __getitem__(self, index):
         img_id = self.ids[index]
+
         ann_ids = self.coco.getAnnIds(imgIds=img_id)
         anns = self.coco.loadAnns(ann_ids)
 
-        # --- Load image ---
+        # ---- Load Image ----
         img_info = self.coco.loadImgs(img_id)[0]
         path = os.path.join(self.img_prefix, img_info["file_name"])
-        img = cv2.imread(path)
+        img = cv.imread(path)
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-        # --- GT boxes ---
+        # ---- Load GT boxes ----
         boxes, labels = [], []
         for ann in anns:
             x, y, w, h = ann["bbox"]
-            boxes.append([x, y, x + w, y + h])  # xyxy
+            boxes.append([x, y, x + w, y + h])  # convert to xyxy format
             labels.append(self.cat_id_to_label[ann["category_id"]])
 
         boxes = torch.tensor(boxes, dtype=torch.float32) if boxes else torch.zeros((0, 4))
         labels = torch.tensor(labels, dtype=torch.long) if labels else torch.zeros((0,), dtype=torch.long)
 
-        img_tensor, scale, pad_shape = self.preprocess(img)
-        ih, iw = pad_shape
-        img_meta = {
-            "ori_shape": img.shape,
-            "img_shape": (ih, iw, 3),
-            "pad_shape": (ih, iw, 3),
-            "scale_factor": np.array([scale, scale, scale, scale], dtype=np.float32),
-            "flip": False,
-            "flip_direction": None,
-            "ori_filename": img_info["file_name"],
-        }
+        # ---- Preprocessing (letterbox) ----
+        rescaled_img, scale = self.preprocess(img)
+        img_tensor = torch.from_numpy(rescaled_img).permute(2, 0, 1).float()
 
+        # ---- Scale GT boxes to resized image coords ----
+        if len(boxes) > 0:
+            boxes = np.array(boxes, dtype=np.float32)
+            boxes *= scale
+            boxes = torch.tensor(boxes, dtype=torch.float32)
+        else:
+            boxes = torch.zeros((0, 4), dtype=torch.float32)
+
+        # ---- Match MOT17 Dataset Target Format ----
         target = {
             "boxes": boxes,
             "labels": labels,
             "image_id": torch.tensor([img_id]),
             "video_id": self.frame_info[img_id]["video_id"],
             "frame_id": self.frame_info[img_id]["frame_id"],
-            "img_metas": img_meta,
+            "img_metas": {
+                "filename": path,
+                "ori_shape": (img_info["height"], img_info["width"], 3),
+                "img_shape": (self.input_size[0], self.input_size[1], 3),
+                "pad_shape": (self.input_size[0], self.input_size[1], 3),
+                "scale_factor": scale,
+            },
         }
 
         return img_tensor, target
 
     def preprocess(self, img):
-        h, w = img.shape[:2]
-        ih, iw = self.input_size
-        scale = min(ih / h, iw / w)
-        nh, nw = int(h * scale), int(w * scale)
+        return self.letterbox(img)
 
-        resized = cv2.resize(img, (nw, nh))
-        pad_h = int(np.ceil(nh / 32) * 32)
-        pad_w = int(np.ceil(nw / 32) * 32)
-        canvas = np.full((pad_h, pad_w, 3), 114, dtype=np.uint8)
-        canvas[:nh, :nw, :] = resized
+    def letterbox(self, img, target_size=(640, 640)):
+        h, w = int(target_size[0]), int(target_size[1])
+        padded_img = np.ones((h, w, 3), dtype=np.float32) * 114.0
 
-        tensor = torch.from_numpy(canvas).permute(2, 0, 1).float()
-        return tensor, scale, (pad_h, pad_w)
+        ratio = min(h / img.shape[0], w / img.shape[1])
+        resized_img = cv.resize(
+            img, (int(img.shape[1] * ratio), int(img.shape[0] * ratio)), interpolation=cv.INTER_LINEAR
+        ).astype(np.float32)
+
+        padded_img[: resized_img.shape[0], : resized_img.shape[1]] = resized_img
+        return padded_img, ratio
